2d9c5616b9947f2bb09c92d13c5d3a9bd9345573
Tony <tonywmfreitas@gmail.com>
2016-10-13 17:07:12 -0300

    Atualizando projeto

--- src/main/webapp/lib/Javascript/Script/script.js
+++ src/main/webapp/lib/Javascript/Script/script.js
@@ -1,6 +1,33 @@
-jQuery(document).ready(function() {
-			App.init();
-			OwlCarousel.initOwlCarousel();
-			StyleSwitcher.initStyleSwitcher();
-			ParallaxSlider.initParallaxSlider();
+var $j = jQuery.noConflict();
+
+var user = {
+	username:'',
+	password:'',
+	email:''
+};
+
+var novoLogin = angular.module("novoLogin",[]);
+
+novoLogin.controller("novoLoginCtrl",function($scope){
+	//Validando senhas inconsistentes
+	$scope.cadastrar = function(){
+		if($scope.senha != $scope.senha_confirm) {
+			$j('.erro-senha').show();
+			$scope.erro = "Senhas inconsistentes!!!";
+			$scope.submitForm = null;
+			$j('.submit-sucess').hide();
+			$j('#ir-login').attr("href","#logincad");
+		}
+		else{
+				$j('.submit-sucess').show();
+				$scope.sucess = "Formulário submetido com sucesso!!";
+				$j('.erro-senha').hide();
+				user.username = $scope.usuario;
+				user.password = $scope.senha;
+				user.email = $scope.email;
+				$j('#ir-login').attr("href","page_login.html");
+		}
+	};
 });
+
+
